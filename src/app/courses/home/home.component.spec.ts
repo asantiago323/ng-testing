cfb9e54b2e4c32d83b3f1a5f6f725d299f9fc092
Anthony Santiago <anthony.santiago@caci.com>
2019-06-26 17:15:28 -0400

    added tests for courses Service and card-list

--- src/app/courses/home/home.component.spec.ts
+++ src/app/courses/home/home.component.spec.ts
@@ -22,40 +22,56 @@ describe('HomeComponent', () => {
   let component:HomeComponent;
   let el: DebugElement;
 
-  beforeEach((() => {
+  beforeEach(async(() => {
+    const coursesServiceSpy = jasmine.createSpyObj('CoursesService', ['findAllCourses']);
 
+    TestBed.configureTestingModule({
+      imports: [
+        CoursesModule,
+        NoopAnimationsModule
+      ],
+      providers: [
+        { provide: CoursesService, useValue: coursesServiceSpy}
+      ]
+    })
+    .compileComponents()
+    .then(() => {
+        fixture = TestBed.createComponent(HomeComponent);
+        component = fixture.componentInstance;
+        el = fixture.debugElement;
+    });
 
   }));
 
-  it("should create the component", () => {
+  it('should create the component', () => {
 
     expect(component).toBeTruthy();
 
   });
 
 
-  it("should display only beginner courses", () => {
+  it('should display only beginner courses', () => {
 
     pending();
 
   });
 
 
-  it("should display only advanced courses", () => {
+  it('should display only advanced courses', () => {
 
       pending();
 
   });
 
 
-  it("should display both tabs", () => {
+  it('should display both tabs', () => {
 
     pending();
 
   });
 
 
-  it("should display advanced courses when tab clicked", () => {
+  it('should display advanced courses when tab clicked', () => {
 
     pending();
 
